576534a420badf0b452c6ab8c7ff7d020a72530b
Thomas Holmes <thomasholmes@outlook.com>
2022-05-29 00:03:38 +0100

    Added a better camera.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-//! Illustrates how to create parent-child relationships between entities and how parent transforms
-//! are propagated to their descendants.
+mod camera;
 
 use bevy::prelude::*;
 use rand::Rng;
@@ -9,7 +8,9 @@ fn main() {
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_caterpillars)
+        .add_startup_system(camera::spawn_camera)
         .add_system(caterpillar_system)
+        .add_system(camera::pan_orbit_camera)
         .run();
 }
 
@@ -157,11 +158,6 @@ fn setup_caterpillars(
     // light
     commands.spawn_bundle(PointLightBundle {
         transform: Transform::from_xyz(4.0, 5.0, -4.0),
-        ..default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(5.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
