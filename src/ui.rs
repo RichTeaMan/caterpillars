836299c786202cfc071d89febee101422f565b6f
Thomas Holmes <thomasholmes@outlook.com>
2022-06-08 11:17:14 +0100

    Simplified fps readout.

--- src/ui.rs
+++ src/ui.rs
@@ -109,7 +109,6 @@ pub fn infotext_system(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 pub fn change_text_system(
-    time: Res<Time>,
     diagnostics: Res<Diagnostics>,
     mut query: Query<&mut Text, With<TextChanges>>,
 ) {
@@ -121,15 +120,7 @@ pub fn change_text_system(
             }
         }
 
-        let mut frame_time = time.delta_seconds_f64();
-        if let Some(frame_time_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
-        {
-            if let Some(frame_time_avg) = frame_time_diagnostic.average() {
-                frame_time = frame_time_avg;
-            }
-        }
-
-        text.sections[0].value = format!("{:.1} fps, {:.3} ms/frame", fps, frame_time * 1000.0,);
+        text.sections[0].value = format!("{:.1} fps", fps);
     }
 }
 
