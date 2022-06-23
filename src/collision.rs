6ea7c6a19b1cbd4a9f0aa7dd0587de7bf0b3adab
Thomas Holmes <thomasholmes@outlook.com>
2022-06-23 11:53:34 +0100

    Added is_inside_polygon method.

--- src/collision.rs
+++ src/collision.rs
@@ -19,10 +19,47 @@ pub fn collision_check(vector_a: Vec3, vector_b: Vec3, distance: f32) -> bool {
     return distance > vector_a.distance(vector_b);
 }
 
+pub fn inside_polygon(vector: Vec2, polygon: &[Vec2]) -> bool {
+    // This method counts how many times an imaginary line drawn from the vector eastwards
+    // overlaps lines of the polygon. An odd number of overlaps is considered inside the polygon,
+    // even number (or zero) is not in the polygon.
+
+    let x1 = vector.x;
+    let y1 = vector.y;
+    let x2 = vector.x + 1_000_000.0;
+    let y2 = vector.y;
+
+    let mut lines = Vec::new();
+    for i in 0..(polygon.len() - 1) {
+        lines.push((polygon[i], polygon[i + 1]));
+    }
+    lines.push((polygon[polygon.len() - 1 as usize], polygon[0]));
+
+    let mut intersections = 0;
+    for line in lines {
+        let x3 = line.0.x;
+        let y3 = line.0.y;
+        let x4 = line.1.x;
+        let y4 = line.1.y;
+
+        // taken from wikipedia: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment
+        let t = (((x1 - x3) * (y3 - y4)) - ((y1 - y3) * (x3 - x4)))
+            / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+        let u = (((x1 - x3) * (y1 - y2)) - ((y1 - y3) * (x1 - x2)))
+            / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+
+        // intersection only occurs if 0 <= t <= 1 & 0 <= u <= 1
+        if 0.0 <= t && t <= 1.0 && 0.0 <= u && u <= 1.0 {
+            intersections += 1;
+        }
+    }
+
+    return intersections % 2 != 0;
+}
+
 #[test]
 #[cfg(test)]
 fn collison_check_test_1() {
-
     let vector_a = Vec3::new(0.0, 0.0, 0.0);
     let vector_b = Vec3::new(0.5, 0.0, 0.0);
 
@@ -32,12 +69,9 @@ fn collison_check_test_1() {
     assert_eq!(collides, true);
 }
 
-
-
 #[test]
 #[cfg(test)]
 fn collison_check_test_2() {
-
     let vector_a = Vec3::new(0.0, 0.0, 0.0);
     let vector_b = Vec3::new(-0.5, 0.0, 0.0);
 
@@ -47,12 +81,9 @@ fn collison_check_test_2() {
     assert_eq!(collides, true);
 }
 
-
-
 #[test]
 #[cfg(test)]
 fn collison_check_test_3() {
-
     let vector_a = Vec3::new(0.0, 0.0, 0.0);
     let vector_b = Vec3::new(0.0, 0.0, 0.0);
 
@@ -65,7 +96,6 @@ fn collison_check_test_3() {
 #[test]
 #[cfg(test)]
 fn collison_check_test_4() {
-
     let vector_a = Vec3::new(0.0, 0.0, 0.0);
     let vector_b = Vec3::new(1.1, 0.0, 0.0);
 
@@ -75,11 +105,9 @@ fn collison_check_test_4() {
     assert_eq!(collides, false);
 }
 
-
 #[test]
 #[cfg(test)]
 fn collison_check_test_5() {
-
     let vector_a = Vec3::new(0.0, 0.0, 0.0);
     let vector_b = Vec3::new(1.0, 1.0, 0.0);
 
@@ -87,4 +115,92 @@ fn collison_check_test_5() {
 
     // Check resulting changes
     assert_eq!(collides, false);
+}
+
+#[test]
+#[cfg(test)]
+fn inside_polygon_test_1() {
+    let vector = Vec2::new(0.0, 0.0);
+    let points = [
+        Vec2::new(-1.0, -1.0),
+        Vec2::new(1.0, -1.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(-1.0, 1.0),
+    ];
+
+    let inside = inside_polygon(vector, &points);
+
+    // Check resulting changes
+    assert_eq!(inside, true);
+}
+
+#[test]
+#[cfg(test)]
+fn inside_polygon_test_2() {
+    let vector = Vec2::new(1.1, 0.0);
+    let points = [
+        Vec2::new(-1.0, -1.0),
+        Vec2::new(1.0, -1.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(-1.0, 1.0),
+    ];
+
+    let inside = inside_polygon(vector, &points);
+
+    // Check resulting changes
+    assert_eq!(inside, false);
+}
+
+#[test]
+#[cfg(test)]
+fn inside_polygon_test_3() {
+    let vector = Vec2::new(0.0, 0.5);
+    let points = [
+        Vec2::new(-1.0, -1.0),
+        Vec2::new(1.0, -1.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(0.0, 0.0),
+        Vec2::new(-1.0, 1.0),
+    ];
+
+    let inside = inside_polygon(vector, &points);
+
+    // Check resulting changes
+    assert_eq!(inside, false);
+}
+
+#[test]
+#[cfg(test)]
+fn inside_polygon_test_4() {
+    let vector = Vec2::new(0.0, 0.0);
+    let points = [
+        Vec2::new(-1.0, -1.0),
+        Vec2::new(1.0, -1.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(0.0, 0.25),
+        Vec2::new(-1.0, 1.0),
+    ];
+
+    let inside = inside_polygon(vector, &points);
+
+    // Check resulting changes
+    assert_eq!(inside, true);
+}
+
+#[test]
+#[cfg(test)]
+fn inside_polygon_test_5() {
+    let vector = Vec2::new(0.0, 1.5);
+    let points = [
+        Vec2::new(-1.0, -1.0),
+        Vec2::new(1.0, -1.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(0.0, 0.0),
+        Vec2::new(-1.0, 1.0),
+    ];
+
+    let inside = inside_polygon(vector, &points);
+
+    // Check resulting changes
+    assert_eq!(inside, false);
 }
